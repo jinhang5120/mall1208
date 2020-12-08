108411eac84c14b85f953b4e63448451e4864acd
jinhang5120 <1411647107@qq.com>
2020-12-08 16:41:08 +0800

    1208

---
+++ mall-parent/mall-api/src/test/java/Test01.java
@@ -0,0 +1,10 @@
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.SQLException;
+
+public class Test01 {
+    public static void main(String[] args) throws SQLException {
+        Driver driver = new Driver();
+
+    }
+}
---
+++ mall-parent/mall-product/src/test/java/com/jh/mall/Test01.java
@@ -0,0 +1,12 @@
+package com.jh.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Test01 {
+    @Test
+    public void m1(){
+        System.out.println("aaaaaa");
+    }
+}
