9083b2263bef33eaf40062d6e726d7651a38b088
jinhang5120 <1411647107@qq.com>
2020-12-10 15:13:22 +0800

    1210.1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # mall1208
+CouponMain      8010
+MemberMain      8020
+OrderMain       8030
+ProductMain     8040
+WareMain        8050
---
+++ mall-parent/mall-gateway/src/main/java/com/jh/mall/GateWayMain.java
@@ -0,0 +1,11 @@
+package com.jh.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateWayMain {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain.class,args);
+    }
+}
